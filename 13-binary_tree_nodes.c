14b5b63c3d2ce9f370c3a1f02147b88d8af33054
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-06-01 13:39:45 +0000

    binary tree nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: tree to count the nodes from
+ *
+ * Return: number of nodes counted
+ *         0 if tree is NULL
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodesT = NULL;
+
+	if (tree == NULL)
+		return (0);
+
+	nodesT = binary_tree_nodes(tree->left) +
+		binary_tree_nodes(tree->right);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (nodesT);
+	return (nodesT + 1);
+}
