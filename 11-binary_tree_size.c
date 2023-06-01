59f2494009f73a4d0954aad7d908f22c0912911a
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-06-01 13:33:04 +0000

    binary tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,15 +8,13 @@
  *         0 if tree is NULL
  */
 
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t sizeT = NULL;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	sizeT = binary_tree_size(tree->left) + 1 +
-		binary_tree_size(tree->right);
+	if (!tree->left && !tree->right)
+		return (1);
 
-	return (sizeT);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
