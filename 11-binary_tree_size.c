8e603a2050981565624a289a0b7141157643b22d
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-06-01 13:35:56 +0000

    binary tree size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,13 +8,10 @@
  *         0 if tree is NULL
  */
 
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
 
-	if (!tree->left && !tree->right)
-		return (1);
-
-	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
