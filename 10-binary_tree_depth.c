ab2f5e1b67893c5afc36377011d6940fcd2289f4
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-06-01 14:00:11 +0000

    binary tree depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -10,15 +10,16 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t prof = NULL;
+	size_t depth = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	if (tree->parent == NULL)
-		return (0);
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
 
-	prof = binary_tree_depth(tree->parent) + 1;
-
-	return (prof);
+	return (depth);
 }
