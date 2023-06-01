33111a2faf04a17a512056529271a0e8710d2351
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2023-06-01 13:34:29 +0000

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,18 +8,3 @@
  *         0 if tree is NULL
  */
  
- size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	size_t leavesT = NULL;
-
-	if (tree == NULL)
-		return (0);
-
-	if (tree->left == NULL && tree->right == NULL)
-		return (1);
-
-	leavesT = binary_tree_leaves(tree->left) +
-		binary_tree_leaves(tree->right);
-
-	return (leavesT);
-}
